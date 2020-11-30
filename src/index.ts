ba123714f79302cf4e31240f1bd860003fa0f442
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2020-11-30 23:31:51 +0200

    Prettier config. Yargs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-interface CLIArguments {}
+import yargs, { Arguments } from "yargs"
 
-function main(args: CLIArguments) {}
+interface CLIArguments {
+  find: boolean
+}
+
+function main(args: CLIArguments) {
+  if (args.find) {
+    console.log("Finding unused translations...")
+  }
+}
+
+yargs
+  .scriptName("react18i-next-cleaner")
+  .usage("$0 <cmd> [args]")
+  .command(
+    "*",
+    "scan your project for unused translations",
+    (yargs) => {
+      yargs.option("find", {})
+    },
+    function (argv: Arguments<CLIArguments>) {
+      return main({ find: argv.find })
+    }
+  )
